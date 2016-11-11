c6c94eb83f8145936f0887dedc163cbfc5de7e29
TheMejmun <saman14m@gmail.com>
2016-11-11 22:36:55 +0100

    Set up first activity and starting setup Activity

--- Honeypot/app/src/main/java/de/honeypot/honeypot/MainActivity.java
+++ Honeypot/app/src/main/java/de/honeypot/honeypot/MainActivity.java
@@ -1,5 +1,8 @@
 package de.honeypot.honeypot;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +11,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SharedPreferences sharedPref = getSharedPreferences("settings", Context.MODE_PRIVATE);
+        String userName = sharedPref.getString("userName", "");
+
+        // no saved userName
+        if(userName.equals("")){
+            Intent firstLaunchActivity = new Intent(this, FirstLaunchActivity.class);
+            startActivity(firstLaunchActivity);
+            finish();
+        }
+
         setContentView(R.layout.activity_main);
     }
 }
