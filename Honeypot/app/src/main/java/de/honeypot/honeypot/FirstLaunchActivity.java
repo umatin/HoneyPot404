97ca56f50baf6aafe5ae6c053935feb14b93b8b6
TheMejmun <saman14m@gmail.com>
2016-11-12 17:15:39 +0100

    Major modifications to FirstLaunchActivity

--- Honeypot/app/src/main/java/de/honeypot/honeypot/FirstLaunchActivity.java
+++ Honeypot/app/src/main/java/de/honeypot/honeypot/FirstLaunchActivity.java
@@ -6,12 +6,22 @@ import android.content.SharedPreferences;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.JsonReader;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ViewSwitcher;
 //import org.apache.http.impl.client.DefaultHttpClient;
 
 import org.apache.http.HttpResponse;
@@ -19,176 +29,209 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
-import static android.R.attr.duration;
-import static java.security.AccessController.getContext;
 
 public class FirstLaunchActivity extends AppCompatActivity {
 
-    private EditText NameField;
+    private static EditText nameField;
 
-    private SharedPreferences sharedPref;
+    private static Button button;
 
-    private SharedPreferences.Editor editor;
+    private static SharedPreferences sharedPref;
 
-    // //Toast.makeText(HelloFormStuff.this, edittext.getText(), Toast.LENGTH_SHORT).show();
+    private static SharedPreferences.Editor editor;
+
+    private static RelativeLayout relativeLayout;
+
+    private static Animation fadeIn, fadeOut;
+
+    private static int randomCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_launch);
 
-
+        Toast.makeText(getApplication(), "test1", Toast.LENGTH_SHORT).show();
         sharedPref = getSharedPreferences("settings", Context.MODE_PRIVATE);
+        nameField = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+        relativeLayout = (RelativeLayout) findViewById(R.id.activity_first_launch);
 
+        // animations
+        fadeIn = new AlphaAnimation(0, 1);
+        fadeIn.setInterpolator(new DecelerateInterpolator()); //add this
+        fadeIn.setDuration(1500);
+        fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setInterpolator(new DecelerateInterpolator()); //add this
+        fadeOut.setDuration(1500);
 
+        // set invisible
+        nameField.setVisibility(View.INVISIBLE);
+        button.setVisibility(View.INVISIBLE);
 
+        final TextSwitcher textSwitcher = (TextSwitcher) findViewById(R.id.textView);
+        textSwitcher.setInAnimation(fadeIn);
+        textSwitcher.setOutAnimation(fadeOut);
 
-        //String userName = sharedPref.getString("userName", "");
+        textSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
 
+            public View makeView() {
+                TextView Text = new TextView(FirstLaunchActivity.this);
+                Text.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL);
+                Text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 36);
+                return Text;
+            }
+        });
 
-        NameField = (EditText) findViewById(R.id.namefield);
-        NameField.setOnKeyListener(new View.OnKeyListener() {
+        textSwitcher.setText(getResources().getString(R.string.first_launch1));
+
+        relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (randomCount){
+                    case 0:
+                        textSwitcher.setText(getResources().getString(R.string.first_launch2));
+                        break;
+                    case 1:
+                        textSwitcher.setText(getResources().getString(R.string.first_launch3));
+                        break;
+                    case 2:
+                        nameField.setVisibility(View.VISIBLE);
+                        button.setVisibility(View.VISIBLE);
+                        break;
+                }
+                randomCount++;
+            }
+        });
+
+        // enter key
+        nameField.setOnKeyListener(new View.OnKeyListener() {
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 // If the event is a key-down event on the "enter" button
                 if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
                         (keyCode == KeyEvent.KEYCODE_ENTER)) {
                     // Perform action on key press
-
-                    final String name = NameField.getText().toString();
-
-                    final String android_id = Settings.Secure.getString(getContentResolver(),
-                            Settings.Secure.ANDROID_ID);
-
-
-                    editor = sharedPref.edit();
-                    editor.putString("userName", name);
-                    editor.putString("device", android_id);
-
-
-
-                    editor.apply();
-
-                    Log.i("name", name);
-                    Log.i("device", android_id);
-
-
-                    //TODO alles auf die cloud laden...
-
-
-
-                    Thread t2 = new Thread() {  //HTTP get anfrage über einen eigenen Thread
-                        public void run() {
-
-
-                            HttpClient client = new DefaultHttpClient();
-
-                            String link = "http://honeypot4431.cloudapp.net";
-                            HttpGet request = new HttpGet(link + "/register?device=" + android_id + "&name=" + name);
-                            // replace with your url
-
-
-                            HttpResponse response;
-                            try {
-                                response = client.execute(request);
-                                //ResponseUtils.toString(response);
-
-                                ByteArrayOutputStream out = new ByteArrayOutputStream();
-                                response.getEntity().writeTo(out);
-                                String responseString = out.toString();
-                                out.close();
-
-                                Log.i("Response of GET request", responseString);
-                                //JsonReader reader = new JsonReader(response);
-                                JSONObject jObject = new JSONObject(responseString);
-
-                                String token = jObject.getString("token");
-                                String id = jObject.getString("id");
-
-                                Log.i("token", token);
-                                Log.i("id", id);
-
-
-                                //SharedPreferences.Editor editor = sharedPref.edit();
-                                FirstLaunchActivity.this.editor.putString("token", token);
-                                editor.putString("id", id);
-
-
-                                editor.apply();
-
-
-
-                            } catch (ClientProtocolException e) {  // falls kein Internet --> zurück
-
-                                e.printStackTrace();
-                                editor.putString("userName", "");
-                                editor.apply();
-                                //return false;
-                                //openIntent();
-                                FirstLaunchActivity.this.runOnUiThread(new Runnable() {
-                                    public void run() {
-                                        Toast.makeText(FirstLaunchActivity.this, "Do hasch kein Internet", Toast.LENGTH_SHORT).show();
-                                    }
-                                });
-
-                                return;
-
-                                //Problem: fehler abfangen
-                            } catch (IOException e) {
-
-                                e.printStackTrace();
-                                //return false;
-                                //openIntent();
-
-                                editor.putString("userName", "");
-                                editor.apply();
-
-                                FirstLaunchActivity.this.runOnUiThread(new Runnable() {
-                                    public void run() {
-                                        Toast.makeText(FirstLaunchActivity.this, "Do hasch kein Internet", Toast.LENGTH_SHORT).show();
-                                    }
-                                });
-
-
-                                return;
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                                Log.i("Server Fehler", "");
-                            }
-                            openIntent();
-
-                        }
-                    };
-
-
-                    t2.start();
-
-
-
-
-
-
+                    keyPress();
                     return true;
                 }
                 return false;
             }
         });
 
+        // button
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                keyPress();
+            }
+        });
 
     }
 
-    private void openIntent(){
+    // handle key press
+    private void keyPress() {
+        final String name = nameField.getText().toString();
+
+        final String android_id = Settings.Secure.getString(getContentResolver(),
+                Settings.Secure.ANDROID_ID);
+
+        editor = sharedPref.edit();
+        editor.putString("userName", name);
+        editor.putString("device", android_id);
+
+        editor.apply();
+
+        Log.i("name", name);
+        Log.i("device", android_id);
+
+        //TODO alles auf die cloud laden...
+
+        Thread t2 = new Thread() {  //HTTP get anfrage über einen eigenen Thread
+            public void run() {
+
+                HttpClient client = new DefaultHttpClient();
+
+                String link = "http://honeypot4431.cloudapp.net";
+                HttpGet request = new HttpGet(link + "/register?device=" + android_id + "&name=" + name);
+                // replace with your url
+
+
+                HttpResponse response;
+                try {
+                    response = client.execute(request);
+                    //ResponseUtils.toString(response);
+
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    response.getEntity().writeTo(out);
+                    String responseString = out.toString();
+                    out.close();
+
+                    Log.i("Response of GET request", responseString);
+                    //JsonReader reader = new JsonReader(response);
+                    JSONObject jObject = new JSONObject(responseString);
+
+                    String token = jObject.getString("token");
+                    String id = jObject.getString("id");
+
+                    Log.i("token", token);
+                    Log.i("id", id);
+
+                    //SharedPreferences.Editor editor = sharedPref.edit();
+                    FirstLaunchActivity.this.editor.putString("token", token);
+                    editor.putString("id", id);
+
+                    editor.apply();
+
+                } catch (ClientProtocolException e) {  // falls kein Internet --> zurück
+
+                    e.printStackTrace();
+                    editor.putString("userName", "");
+                    editor.apply();
+                    //return false;
+                    //openIntent();
+                    FirstLaunchActivity.this.runOnUiThread(new Runnable() {
+                        public void run() {
+                            Toast.makeText(FirstLaunchActivity.this, "Do hasch kein Internet", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                    return;
+
+                    //Problem: fehler abfangen
+                } catch (IOException e) {
+
+                    e.printStackTrace();
+                    //return false;
+                    //openIntent();
+
+                    editor.putString("userName", "");
+                    editor.apply();
+
+                    FirstLaunchActivity.this.runOnUiThread(new Runnable() {
+                        public void run() {
+                            Toast.makeText(FirstLaunchActivity.this, "Do hasch kein Internet", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                    return;
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Log.i("Server Fehler", "");
+                }
+                openIntent();
+
+            }
+        };
+
+        t2.start();
+    }
+
+    private void openIntent() {
         FirstLaunchActivity.this.runOnUiThread(new Runnable() {
             public void run() {
                 Intent mainActivity = new Intent(FirstLaunchActivity.this, MainActivity.class);
@@ -204,4 +247,6 @@ public class FirstLaunchActivity extends AppCompatActivity {
         super.onPause();
         finish();
     }
+
+
 }
