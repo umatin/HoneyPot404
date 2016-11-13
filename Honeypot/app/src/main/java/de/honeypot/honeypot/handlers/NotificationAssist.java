7365a819bf099fca8ef1681e2edda4c4db378674
TheMejmun <saman14m@gmail.com>
2016-11-13 14:11:05 +0100

    ProfileLoader

--- Honeypot/app/src/main/java/de/honeypot/honeypot/handlers/NotificationAssist.java
+++
@@ -1,9 +0,0 @@
-package de.honeypot.honeypot.handlers;
-
-/**
- * Created by Saman on 12.11.2016.
- */
-
-public class NotificationAssist {
-
-}
---
+++ Honeypot/app/src/main/java/de/honeypot/honeypot/handlers/ProfileLoader.java
@@ -0,0 +1,16 @@
+package de.honeypot.honeypot.handlers;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import de.honeypot.honeypot.R;
+
+public class ProfileLoader extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_loader);
+    }
+}
