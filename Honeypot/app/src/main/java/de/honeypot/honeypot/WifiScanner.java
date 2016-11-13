ea0d0137ab681cf3e685e8979507f0e2aa39115b
JobiProGrammer <tobias@juelg.net>
2016-11-13 10:33:31 +0100

    New Wifi scanner methode

---
+++ Honeypot/app/src/main/java/de/honeypot/honeypot/WifiScanner.java
@@ -0,0 +1,62 @@
+package de.honeypot.honeypot;
+
+import android.content.Context;
+import android.net.wifi.ScanResult;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import de.honeypot.honeypot.handlers.WlanDetection;
+
+/**
+ * Created by Tobias on 13.11.2016.
+ */
+
+public class WifiScanner extends Thread{ // Background thread which gives an event if a network could be a pi
+
+    private boolean abbruch=false;
+    private WlanDetection wifi;
+    private Context mContext;
+
+
+    private final static Logger logger = Logger.getLogger("WlanDetection");
+
+    public WifiScanner(Context context){
+        mContext=context;
+        wifi = new WlanDetection(mContext);
+        start();
+
+    }
+
+    public void run(){
+
+        while (!abbruch){
+            wifi.StartSearching();
+            List<ScanResult> wifiresults = wifi.getResults();
+
+            for(int i =0; i<wifiresults.size(); i++){
+                if (wifiresults.get(i).SSID.substring(0, 7).equals("honeypot") ){
+                    //wifi detected --> conquer
+                    logger.info("Found Wifi Network "+ wifiresults.get(i).SSID);
+
+                }
+            }
+
+
+
+        }
+    }
+
+
+    public void stopService(){
+        abbruch=true;
+    }
+
+    public void startService(){
+        abbruch=false;
+        start();
+    }
+
+
+
+}
